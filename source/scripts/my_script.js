4799c4d7a8610c77a5be0a096f36d36ca96d1baf
Florian <florian.maley26@gmail.com>
2020-02-05 11:32:18 +0100

    Add button

--- source/scripts/my_script.js
+++ source/scripts/my_script.js
@@ -5,3 +5,14 @@
 function init(){
     document.getElementById("test").innerHTML = "init says Hi!";
 }
+
+/*
+*Test_button
+*called by button clicked
+*/
+function test_button(){
+    var my_test_div = document.getElementById("test");
+    my_test_div.innerHTML = "Button clicked!";
+    my_test_div.style.backgroundColor = "green"
+    alert("Clicked")
+}
